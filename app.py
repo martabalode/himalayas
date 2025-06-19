b65842130ecb351e86b953e69847631843af9de1
Marta Balode <46479406+martabalode@users.noreply.github.com>
2025-06-19 18:07:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -270,9 +270,9 @@ if st.button("🚀 Confirm and Continue"):
     
     # Here we need to run model 1 to get max_height
 
-    scaler_path = hf_hub_download(repo_id="martaimarta/himalayamodels", filename="scaler.joblib")
-    encoder_path = hf_hub_download(repo_id="martaimarta/himalayamodels", filename="encoder (2).joblib")
-    model_path = hf_hub_download(repo_id="martaimarta/himnew", filename="model.joblib")
+    scaler_path = hf_hub_download(repo_id="martaimarta/himalayamodels", filename="scaler.joblib",  token="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")
+    encoder_path = hf_hub_download(repo_id="martaimarta/himalayamodels", filename="encoder (2).joblib",token="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")
+    model_path = hf_hub_download(repo_id="martaimarta/himnew", filename="model.joblib", token="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")
 
     scaler = load(scaler_path)
     encoder = load(encoder_path) #'encoder (2).joblib'
@@ -348,9 +348,9 @@ if st.button("🚀 Confirm and Continue"):
 
 
 
-    scaler_path2 = hf_hub_download(repo_id="martaimarta/himalayamodels", filename="scaler2.joblib")
-    encoder_path2 = hf_hub_download(repo_id="martaimarta/himalayamodels", filename="encoder2.joblib")
-    model_path2 = hf_hub_download(repo_id="martaimarta/himnew", filename="model2.joblib")
+    scaler_path2 = hf_hub_download(repo_id="martaimarta/himalayamodels", filename="scaler2.joblib",token="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")
+    encoder_path2 = hf_hub_download(repo_id="martaimarta/himalayamodels", filename="encoder2.joblib",token="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")
+    model_path2 = hf_hub_download(repo_id="martaimarta/himnew", filename="model2.joblib",token="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")
 
     scaler2 = load(scaler_path2)
     encoder2 = load(encoder_path2) #'encoder (2).joblib'
