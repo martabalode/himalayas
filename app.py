1ebd0ca9dc1379b4e8d1c870dfc2abb6d915f6bd
Marta Balode <46479406+martabalode@users.noreply.github.com>
2025-06-19 18:32:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -444,7 +444,7 @@ if st.button("🚀 Confirm and Continue"):
 
     
     import plotly.express as px
-    fig=px.scatter_map(output,lat='latitude', lon= 'longitude',size='success_rate',color='success_prob',hover_data='pkname',color_continuous_scale='RdYlGn',)
+    fig=px.scatter_map(output,lat='latitude', lon= 'longitude',size='success_prob',color='success_prob',hover_data='pkname',color_continuous_scale='RdYlGn',)
     fig.update_layout(map_style="open-street-map", mapbox_center={"lat": mean_lat, "lon": mean_lon})
     fig.show()    
     st.plotly_chart(fig)
